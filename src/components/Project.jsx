4838eada341346b1ae0a783cefde73b9c112a7d7
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-17 10:57:20 +0530

    minor changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,7 +13,7 @@ const Project = () => {
                         <div className="card-body">
                             <h5 className="card-title">E-Commerce Web Application</h5>
                             <p className="card-text">Developed an E-Commerce website using the Django framework along with CSS3 and Bootstrap4 for frontend. Implemented authentication system for users to register and login in the E-Commerce website. Implemented the cart and checkout feature for the application</p>
-                            <a href="https://github.com/SaiVamshiK/My-Store">Link to Repo</a>
+                            <a href="https://github.com/SaiVamshiK/My-Store" target="blank">Link to Repo</a>
                         </div>
                     </div>
                 </div>
@@ -23,7 +23,7 @@ const Project = () => {
                         <div className="card-body">
                             <h5 className="card-title">Transport Pooling Application</h5>
                             <p className="card-text">Developed an Android application for farmers to reduce the cost of transportation of crop, using the idea of pooling rides. Implemented the feature of pooling rides based on proximity of locations.</p>
-                            <a href="https://github.com/SaiVamshiK/pooling-app">Link to Repo</a>
+                            <a href="https://github.com/SaiVamshiK/pooling-app" target="blank">Link to Repo</a>
                         </div>
                     </div>
                 </div>
@@ -36,7 +36,7 @@ const Project = () => {
                         <div className="card-body">
                             <h5 className="card-title">Real Time Face Mask Detector</h5>
                             <p className="card-text">Developed a deep learning model that detects the humans wearing a face mask in video streams. The model is developed using the Convolutional Neural Networks. Opencv is used for face detection.</p>
-                            <a href="https://github.com/SaiVamshiK/Real-Time-Face-Mask-Detector">Link to Repo</a>
+                            <a href="https://github.com/SaiVamshiK/Real-Time-Face-Mask-Detector" target="blank">Link to Repo</a>
                         </div>
                     </div>
                 </div>
@@ -46,7 +46,7 @@ const Project = () => {
                         <div className="card-body">
                             <h5 className="card-title">Covid-19 Future Predictor</h5>
                             <p className="card-text">Developed a Website that provides live updates about the Covid-19 pandemic in India using an API. Implemented the feature of providing future predictions for every state in India.</p>
-                            <a href="https://github.com/SaiVamshiK/Covid19-Future-Predictions-in-India">Link to Repo</a>
+                            <a href="https://github.com/SaiVamshiK/Covid19-Future-Predictions-in-India" target="blank">Link to Repo</a>
                         </div>
                     </div>
                 </div>
@@ -59,7 +59,7 @@ const Project = () => {
                         <div className="card-body">
                             <h5 className="card-title">Traffic Predictor Web Application</h5>
                             <p className="card-text">Developed a web portal to predict the traffic volume on the uploaded data using a python-based traffic volume predictor. Implemented features of user authentication and user profile management using ASP.NET Core MVC. The predictions are made based on the parameters like weather, time of the day, temperature, day of the week, etc.</p>
-                            <a href="https://github.com/SaiVamshiK/Traffic-Predictor-Deloitte-Project">Link to Repo</a>
+                            <a href="https://github.com/SaiVamshiK/Traffic-Predictor-Deloitte-Project" target="blank">Link to Repo</a>
                         </div>
                     </div>
                 </div>
@@ -69,7 +69,7 @@ const Project = () => {
                         <div className="card-body">
                             <h5 className="card-title">Data structures in C++</h5>
                             <p className="card-text">Solving problems using Data structures in C++.</p>
-                            <a href="https://github.com/SaiVamshiK/Data-Structure-in-C-/tree/main">Link to Repo</a>
+                            <a href="https://github.com/SaiVamshiK/Data-Structure-in-C-/tree/main" target="blank">Link to Repo</a>
                         </div>
                     </div>
                 </div>
